1ef5a640b80a482364919ba56e48efe654430a4c
Naomiluv02 <naomiluv02@yahoo.com>
2024-07-04 10:28:05 -0400

    Update and rename Inventory_managementcodes to IMS_Json_codes.py

---
+++ IMS_Json_codes.py
@@ -0,0 +1,72 @@
+{
+    "Apple": 200,
+    "Electronics": {
+        "MacBook Laptops": 300,
+        "Laptop": 10,
+        "Smartphone": 20,
+        "Tablet": 15,
+        "Headphone": 25,
+        "Smartwatch": 12
+    },
+    "Groceries": {
+        "Apples": 50,
+        "Bananas": 60,
+        "Milk": 30,
+        "Bread": 40,
+        "Eggs": 100
+    },
+    "Office Supplies": {
+        "Notebook": 100,
+        "Pen": 200,
+        "Stapler": 30,
+        "Printer Paper": 500,
+        "Binder": 50
+    },
+    "Clothing": {
+        "T-Shirt": 30,
+        "Jeans": 25,
+        "Jacket": 15,
+        "Sneakers": 20,
+        "Socks": 100
+    },
+    "Household Items": {
+        "Dish Soap": 40,
+        "Toilet Paper": 100,
+        "Laundry Detergent": 25,
+        "Light Bulbs": 100,
+        "Cleaning Spray": 30
+    },
+    "Toys": {
+        "Action Figure": 50,
+        "Board Game": 20,
+        "Puzzle": 30,
+        "Toy Car": 40,
+        "Doll": 25
+    },
+    "Books": {
+        "Fiction Novel": 20,
+        "Non-Fiction Book": 15,
+        "Children's Book": 30,
+        "Cookbook": 10,
+        "Magazine": 50
+    },
+    "Beauty Products": {
+        "Shampoo": 25,
+        "Condotioner": 25,
+        "Lotion": 40,
+        "Toothpaste": 30,
+        "Soap": 50
+    },
+    "Pet Supplies": {
+        "Dog Food": 20,
+        "Cat Litter": 15,
+        "Bird Seed": 25,
+        "Fish Food": 30,
+        "Pet Shampoo": 30
+    },
+    "Movies": {
+        "Action": 30,
+        "Comedy": 90
+    }
+}
+
